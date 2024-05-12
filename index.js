87443ebaa5eb06f5e7ebad052cc8bf78e158f724
tahmedofficial <tanvirofcuo@gmail.com>
2024-05-12 17:40:01 +0600

    finish single ordered food delete api

--- index.js
+++ index.js
@@ -115,6 +115,12 @@ async function run() {
             res.send(result)
         })
 
+        app.delete("/foodOrder/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
+        })
 
 
         // Send a ping to confirm a successful connection

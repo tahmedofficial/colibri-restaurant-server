8e216104c05be56178e4b69516626926e3ccf28d
tahmedofficial <tanvirofcuo@gmail.com>
2024-05-13 00:42:06 +0600

    finish deploy in vercel

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+            "https://colibri-restaurant.web.app",
+            "https://colibri-restaurant.firebaseapp.com",
+        ],
+        credentials: true,
+    })
+);
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.BD_USER}:${process.env.BD_PASS}@cluster0.ufkobjs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -155,7 +164,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
